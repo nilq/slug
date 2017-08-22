fee79c27d22ae6ed4350aa7ded97cafec8a2b2c5
nilq <i.am@nilq.dk>
2017-08-22 09:06:43 +0200

    Add lexer and simple test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod slug;
+
+use slug::syntax;
+use syntax::lexer::{BlockTree, process_branch};
+
 fn main() {
-    println!("Hello, world!");
+    let test = r#"
+a num = 10
+    "#;
+    
+    let mut blocks = BlockTree::new(test, 0);
+    let indents    = blocks.indents();
+
+    let root = blocks.tree(&indents);
+    let done = process_branch(&root);
+    
+    println!("#{:#?}", done);
 }
