7ace25ff90c90afa2a6af1f0ab5a0bfd55adaeaa
nilq <i.am@nilq.dk>
2017-08-22 12:32:06 +0200

    Implement basic parser

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod slug;
 
 use slug::syntax;
 use syntax::lexer::{BlockTree, process_branch};
+use syntax::parser::{Traveler, Parser};
 
 fn main() {
     let test = r#"
@@ -13,6 +14,16 @@ a num = 10
 
     let root = blocks.tree(&indents);
     let done = process_branch(&root);
+    
+    let traveler = Traveler::new(done.clone());
+    let mut parser = Parser::new(traveler);
+
+    match parser.parse() {
+        Err(why)  => println!("error: {}", why),
+        Ok(stuff) => {
+            println!("{:#?}", stuff)
+        },
+    }
     
     println!("#{:#?}", done);
 }
