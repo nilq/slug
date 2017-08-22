94eb4ed86b3066b2af880dd02e574cedae3be40d
nilq <i.am@nilq.dk>
2017-08-22 08:52:50 +0200

    Add tokenizer and matcher

--- src/slug/syntax/lexer/mod.rs
+++ src/slug/syntax/lexer/mod.rs
@@ -1,5 +1,9 @@
 pub mod token;
 pub mod block_tree;
+pub mod tokenizer;
+pub mod matcher;
 
 pub use self::token::*;
 pub use self::block_tree::*;
+pub use self::tokenizer::*;
+pub use self::matcher::*;
